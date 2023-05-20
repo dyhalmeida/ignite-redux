7970c2744656fae401774a021c38c9cc0ef19a76
dyhalmeida <dyhalmeida@gmail.com>
2023-05-20 11:08:55 -0300

    feat: add immer to handle immutable state

--- src/data/store/cart/reducer.ts
+++ src/data/store/cart/reducer.ts
@@ -1,4 +1,5 @@
 import { Reducer } from "redux"
+import { produce } from 'immer'
 import { ICartState } from "./interfaces/cart.interface"
 
 const initialState: ICartState = {
@@ -8,23 +9,19 @@ const initialState: ICartState = {
 export const cart: Reducer<ICartState> = (state = initialState, action) => {
   console.log(state, action)
 
-  switch (action.type) {
-    case 'ADD_PRODUCT_TO_CART': {
-      const { product } = action.payload
-      
-      return {
-        ...state,
-        items: [
-          ...state.items,
-          {
-            product,
-            quantity: 1
-          }
-        ]
+  return produce(state, draft => {
+    switch (action.type) {
+      case 'ADD_PRODUCT_TO_CART': {
+        const { product } = action.payload
+        draft.items.push({
+          product,
+          quantity: 1
+        })
+        break
+      }
+      default: {
+        return draft
       }
     }
-    default: {
-      return state
-    }
-  }
+  })
 }
